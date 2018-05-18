87d2649482cdc3dd6d33ce6ee1758124264ef1ca
NikitaUnkind <lliamet@mail.ru>
2018-05-18 17:48:00 +0300

    3th commit

--- TriaCom/js/common.js
+++ TriaCom/js/common.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+    $('#my-menu').mmenu({
+        extensions: ['theme-dark','effect-menu-slide','pagedim-black'],
+        navbar: {
+            title: 'МЕНЮ'
+        },
+        offCanvas:{
+            position: 'left'
+        }
+
+    });
+
+    var api = $('#my-menu').data('mmenu');
+    api.bind('open:finish', function () {
+        $('.hamburger').addClass('is-active');
+    }).bind('close:finish', function () {
+        $('.hamburger').removeClass('is-active');
+    });
+
     $('.carousel-team').owlCarousel({
         loop: true,
         responsiveClass: true,
